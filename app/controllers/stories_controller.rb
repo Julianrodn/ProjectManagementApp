4222dcbbf24bdb6196b6e385d45b6c28c0d359aa
Julian Rodriguez <julian.12@hotmail.es>
2024-06-07 10:58:23 -0500

    feat(app): add styles to the views and settings

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,6 +7,7 @@ class StoriesController < ApplicationController
 
   def new
     @story = @project.stories.new
+    @story.tickets.build  # Construir un ticket dentro de la historia
   end
 
   def create
@@ -14,15 +15,32 @@ class StoriesController < ApplicationController
 
     ActiveRecord::Base.transaction do
       if @story.save
-        redirect_to new_project_story_ticket_path(@project, @story), notice: 'Story created successfully. Please create the first ticket.'
+        # Construir el ticket asociado con los parámetros del ticket
+        puts "#######################################################################"
+        puts "#######################################################################"
+        puts "#######################################################################"
+        puts "#######################################################################"
+        puts ticket_params 
+        puts "#######################################################################"
+        puts "#######################################################################"
+        puts "#######################################################################"
+        @ticket = @story.tickets.new(ticket_params)
+        
+        if @ticket.save
+          redirect_to project_story_path(@project, @story), notice: 'Historia y ticket creados con éxito.'
+        else
+          raise ActiveRecord::Rollback  # Forzar rollback si el ticket no se guarda
+        end
       else
         render :new
       end
     end
+  rescue ActiveRecord::Rollback
+    flash[:alert] = 'Hubo un problema al crear la historia y el ticket.'
+    render :new
   end
 
   def edit
-    @story = Story.find(params[:id])
   end
 
   def update
@@ -50,6 +68,11 @@ class StoriesController < ApplicationController
   end
 
   def story_params
-    params.require(:story).permit(:name, :description) # Ajusta los parámetros según tu modelo de datos
+    params.require(:story).permit(:name, :description)
+  end
+
+  def ticket_params
+    params.require(:story).require(:tickets).permit(:name, :description, :comments, :status)
   end
 end
+
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,7 @@ class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
   has_many :stories
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
